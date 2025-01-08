476dcc45911d744ae3fee1cfebf5e655e3c288e0
Nivedita Vishwakarma <niveditavishwakarma205@gmail.com>
2025-01-08 12:15:18 +0530

    temperature converter completed

---
+++ temperature.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class temperature{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter temp:");
+		float celsius = sc.nextFloat();
+		float fahrenheit = ((celsius * 9/5) + 32);
+		System.out.println("fahrenheit:" + fahrenheit);
+	}
+}
